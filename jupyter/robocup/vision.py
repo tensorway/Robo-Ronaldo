6318891de4adfa03bbd4a0fb5ce62e8564a664b9
tensorway <darijan.gudelj@gmail.com>
2020-03-05 00:51:49 +0100

    vision now works on picamera + mask that tells what is not ball
    vision is on picamera and iso, exposure, white ballance can be controlled
    if there is no ball near robot it can now threshold everything and
    see what is definitelly not ball

--- jupyter/robocup/vision.py
+++ jupyter/robocup/vision.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
-import time as t
 import matplotlib.pyplot as plt
 import math
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+from fractions import Fraction
 
 
 xball = -100
@@ -10,32 +13,66 @@ yball = -100
 rball = -10
 balli = 1
 
+
 def no(x):
     a = 0
 
-def read_trackbars():
-    hl = cv2.getTrackbarPos('h', 'low')
-    sl = cv2.getTrackbarPos('s', 'low')
-    vl = cv2.getTrackbarPos('v', 'low')
+def read_trackbars(l1, h1, l2, h2):
+    h1[0] = cv2.getTrackbarPos('h', 'high1')
+    h1[1] = cv2.getTrackbarPos('s', 'high1')
+    h1[2] = cv2.getTrackbarPos('v', 'high1')
     
-    hh = cv2.getTrackbarPos('h', 'high')
-    sh = cv2.getTrackbarPos('s', 'high')
-    vh = cv2.getTrackbarPos('v', 'high')
+    h2[0] = cv2.getTrackbarPos('h', 'high2')
+    h2[1] = cv2.getTrackbarPos('s', 'high2')
+    h2[2] = cv2.getTrackbarPos('v', 'high2')
+    
+    l1[0] = cv2.getTrackbarPos('h', 'low1')
+    l1[1] = cv2.getTrackbarPos('s', 'low1')
+    l1[2] = cv2.getTrackbarPos('v', 'low1')
+    
+    l2[0] = cv2.getTrackbarPos('h', 'low2')
+    l2[1] = cv2.getTrackbarPos('s', 'low2')
+    l2[2] = cv2.getTrackbarPos('v', 'low2')
 
 def setup_trackbars():
-    cv2.namedWindow('low')
-    cv2.namedWindow('high')
-    cv2.createTrackbar('h', 'low', 0, 179, no)
-    cv2.createTrackbar('s', 'low', 0, 255, no)
-    cv2.createTrackbar('v', 'low', 0, 255, no)
-    cv2.createTrackbar('h', 'high', 0, 179, no)
-    cv2.createTrackbar('s', 'high', 0, 255, no)
-    cv2.createTrackbar('v', 'high', 0, 255, no)
-    cv2.createTrackbar('ratio_low', 'low', 100, 300, no)
-    cv2.createTrackbar('ratio_high', 'high', 100, 300, no)
-    cv2.createTrackbar('epsilon', 'low', 0, 100, no)
-    cv2.createTrackbar('pp_low', 'low', 0, 1000, no)
-    cv2.createTrackbar('pp_high', 'high', 0, 1000, no)
+    cv2.namedWindow('low1')
+    cv2.namedWindow('high1')
+    cv2.createTrackbar('h', 'low1', 0, 180, no)
+    cv2.createTrackbar('s', 'low1', 0, 255, no)
+    cv2.createTrackbar('v', 'low1', 0, 255, no)
+    cv2.createTrackbar('h', 'high1', 0, 180, no)
+    cv2.createTrackbar('s', 'high1', 0, 255, no)
+    cv2.createTrackbar('v', 'high1', 0, 255, no)
+    
+    cv2.namedWindow('low2')
+    cv2.namedWindow('high2')
+    cv2.createTrackbar('h', 'low2', 0, 180, no)
+    cv2.createTrackbar('s', 'low2', 0, 255, no)
+    cv2.createTrackbar('v', 'low2', 0, 255, no)
+    cv2.createTrackbar('h', 'high2', 0, 180, no)
+    cv2.createTrackbar('s', 'high2', 0, 255, no)
+    cv2.createTrackbar('v', 'high2', 0, 255, no)
+
+
+def setup_default_trackbars(l1, h1, l2, h2):
+    cv2.namedWindow('low1')
+    cv2.namedWindow('high1')
+    cv2.setTrackbarPos('h', 'low1', l1[0])
+    cv2.setTrackbarPos('s', 'low1', l1[1])
+    cv2.setTrackbarPos('v', 'low1', l1[2])
+    cv2.setTrackbarPos('h', 'high1', h1[0])
+    cv2.setTrackbarPos('s', 'high1', h1[1])
+    cv2.setTrackbarPos('v', 'high1', h1[2])
+    
+    cv2.namedWindow('low2')
+    cv2.namedWindow('high2')
+    cv2.setTrackbarPos('h', 'low2', l2[0])
+    cv2.setTrackbarPos('s', 'low2', l2[1])
+    cv2.setTrackbarPos('v', 'low2', l2[2])
+    cv2.setTrackbarPos('h', 'high2', h2[0])
+    cv2.setTrackbarPos('s', 'high2', h2[1])
+    cv2.setTrackbarPos('v', 'high2', h2[2])
+
 
 def draw_cross(img, x, y, length, color):
     img = cv2.line(img, (x, y), (x, y + length), color ,5)
@@ -44,27 +81,47 @@ def draw_cross(img, x, y, length, color):
     img = cv2.line(img, (x, y), (x - length, y), color ,5)
     return img
 
-def detect_the_ball(anglenum, dist, sizenum, debug):
-    cap = cv2.VideoCapture(0)
-    #cap.set(3,1280);
-    #cap.set(4,720);
-    #cap.set(4,480)
-    #cap.set(3,640)
-    #cap.set(3,320);
-    #cap.set(4,240);
-    __, img = cap.read()
-    while(img is None):
-        __, img = cap.read()
-    __, img = cap.read()
-    ymax, xmax, _ = img.shape
-    print(img.shape)
-    if debug:
-        setup_trackbars()
-        lowi = np.zeros((200, 200, 3), np.uint8)
-        highi = np.zeros((200, 200, 3), np.uint8)
-    hl = sl = vl = hh = sh = vh = cx = cy = 0
-    hight = [hh, sh, vh]
-    lowt = [hl, sl, vl]
+
+def set_camera_modes(camera, res):
+    print(camera.resolution)
+    camera.resolution = res
+    print(camera.resolution)
+    #camera.framerate = 30
+
+    time.sleep(1)
+    camera.iso = 150
+    t = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = t
+    #camera.digital_gain = Fraction(1, 1)
+    camera.drc_strength = 'off'
+    camera.exposure_mode = 'off'
+    
+def create_not_mask(nnmask, noise):
+    nnoise = cv2.bitwise_not(noise)
+    nnmask = cv2.bitwise_and(nnmask, nnoise)
+    return nnmask
+    
+    
+def detect_the_ball(anglenum, dist, sizenum, recnum, erasenum, debug):
+    
+    camera = PiCamera()
+    res = (640, 480)
+    camera.resolution = res
+    #camera.framerate = 30
+    nnmask = cv2.imread('nnmask.png', 0)
+
+    set_camera_modes(camera, res)
+    rawCapture = PiRGBArray(camera, size=res)
+
+    time.sleep(0.1)
+    n = 0
+    t1 = time.time()
+    averf = 10
+    nmask = 0
+    angle = distance = 0
+    ymax, xmax = res
+
     xball = -100
     yball = -100
     rball = -10
@@ -73,67 +130,43 @@ def detect_the_ball(anglenum, dist, sizenum, debug):
     last_size_mode = 1
     xcen = 303
     ycen = 231
-    t2 = t.time()
+    wrote = True
+    t2 = time.time()
+    
+    low1 = np.array([0, 120, 60], np.uint8)
+    low2 = np.array([170, 120, 60], np.uint8)
+    high1 = np.array([10, 255, 255], np.uint8)
+    high2 = np.array([180, 255, 255], np.uint8)
+    
+    if debug:
+        setup_trackbars()
+        setup_default_trackbars(low1, high1, low2, high2)
 
-    while True:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        img = frame.array
         distance = -1
         ball_ret_size = -1
         reliable = -1
         ball_size = -1
-        if size_mode != last_size_mode:
-            if size_mode == 1:
-                cap.set(3,1280);
-                cap.set(4,720); 
-            else:
-                cap.set(4,480)
-                cap.set(3,640)     
-            last_size_mode = size_mode
-        __, img = cap.read()
+        
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hight = np.array([1, 255, 255])
-        lowt = np.array([0, 49, 142])
-        hight2 = np.array([180, 255, 255])
-        lowt2 = np.array([168, 49, 142])
-        if debug:
-            hl = cv2.getTrackbarPos('h', 'low')
-            sl = cv2.getTrackbarPos('s', 'low')
-            vl = cv2.getTrackbarPos('v', 'low')
-
-            hh = cv2.getTrackbarPos('h', 'high')
-            sh = cv2.getTrackbarPos('s', 'high')
-            vh = cv2.getTrackbarPos('v', 'high')
-
-            low_ratio = cv2.getTrackbarPos('ratio_low', 'low')/100
-            high_ratio = cv2.getTrackbarPos('ratio_high', 'high')/100
-            e = cv2.getTrackbarPos('epsilon', 'low')/100
-            low_pp = cv2.getTrackbarPos('pp_low', 'low')
-            high_pp = cv2.getTrackbarPos('pp_high', 'high')
-
-            #cv2.circle(img,(350, 220), 100, (0,0,0), -1)
-
-            if 0 != 0:
-                hight = np.array([hh, sh, vh], np.uint8)
-            highi[:] = [hh, sh, vh]
-            highc = cv2.cvtColor(highi, cv2.COLOR_HSV2BGR)
-
-            if 0 != 0:
-                lowt = np.array([hl, sl, vl], np.uint8)
-            lowi[:] = [hl, sl, vl]
-            lowc = cv2.cvtColor(lowi, cv2.COLOR_HSV2BGR)
-
-
-
-        #mask = cv2.inRange(hsv, lowt, hight)
-        mask1 = cv2.inRange(hsv, lowt, hight)
-        mask2 = cv2.inRange(hsv, lowt2, hight2)
+        mask1 = cv2.inRange(hsv, low1, high1)
+        mask2 = cv2.inRange(hsv, low2, high2)
         mask = cv2.bitwise_or(mask1, mask2)
-        kernel = np.ones((5, 5), np.uint8)/25
-        mask = cv2.erode(mask, kernel, 1)
-        kernel = np.ones((5, 5), np.uint8)/25
-        mask = cv2.dilate(mask, kernel, 1)
-        #mask = proc
+        mask = cv2.bitwise_and(mask, nnmask)
+        mask = cv2.erode(mask, np.ones((4, 4))/16)
+        mask = cv2.dilate(mask, np.ones((4, 4))/16)
         res = cv2.bitwise_and(img,img,mask = mask)
         
+        if recnum.value >0.5:
+            wrote = False
+            nnmask = create_not_mask(nnmask, mask)
+        
+        if erasenum.value > 0.5:
+            nnmask = np.ones(nnmask.shape, np.uint8)*255
+
+        if not wrote:
+            cv2.imwrite('nnmask.png', nnmask)
 
         img2, con, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         if len(con) > 0:
@@ -141,36 +174,24 @@ def detect_the_ball(anglenum, dist, sizenum, debug):
             i_max_size = 0
             for i in range (len(con)):
                 cnt = con[i]
-                '''
-                _, (wr, hr), _ = cv2.minAreaRect(cnt)
-                ratio = wr/(hr+0.001) if (wr>hr) else hr/(wr+0.001)
-                if ratio > 1.5:
-                    continue
-                area = cv2.contourArea(cnt)
-                rect = cv2.minAreaRect(cnt)
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                res = cv2.drawContours(res,[box],0,(0,0,255),2)'''
                 
                 area = cv2.contourArea(cnt)    
                 (x,y),r = cv2.minEnclosingCircle(cnt)
                 center = (int(x),int(y))
                 radius = int(r)
-                if r < 8:
+                if r < 4:
                     continue
                 round_coeff = area/(r*r*3.14)
-                if round_coeff < 0.43:
+                if round_coeff < 0.30:
                     continue
                 res = cv2.circle(res,center,radius,(0,255,0),2)
-                if area > 10000:
-                    continue
-                area_coeff = area/500 if area<500 else 1
-                #print(area, r, r*r*3.14)
-                if max_size < area_coeff+round_coeff+ball_inc(x, y, r):
-                    max_size = area_coeff+round_coeff+ball_inc(x, y, r)
+                
+                
+                #area_coeff = area/500 if area<500 else 1
+                if max_size < area:#area_coeff+round_coeff+ball_inc(x, y, r):
+                    max_size = area#_coeff+round_coeff+ball_inc(x, y, r)
                     i_max_size = i
-                    #print(area, r, r*r*3.14)
-            #print(max_size, i_max_size)
+
             if max_size > 0:
                 img = cv2.drawContours(img, con[i_max_size], -1, (0,255,0), 1)
                 cnt = con[i_max_size]
@@ -183,24 +204,22 @@ def detect_the_ball(anglenum, dist, sizenum, debug):
                 ball_ret_size = ball_size
                 _,r = cv2.minEnclosingCircle(cnt)
                 distance = dist_from_size(r)
-            ''''    
-            if distance == -1:
-                size_mode = 1
-            if ball_size > 800:
-                size_mode = 0
-            if ball_size < 100:
-                size_mode = 1'''
             
             
-
 
         if debug:
-            #res = cv2.bitwise_and(img,img,mask = mask)
-            cv2.imshow('low', lowc)
-            cv2.imshow('high', highc)
+
+            read_trackbars(low1, high1, low2, high2)
+            #cv2.imshow('low1', cv2.cvtColor(low1, cv2.COLOR_HSV2BGR))
+            #cv2.imshow('high1', cv2.cvtColor(high1, cv2.COLOR_HSV2BGR))
+            #cv2.imshow('low2', cv2.cvtColor(low2, cv2.COLOR_HSV2BGR))
+            #cv2.imshow('high2', cv2.cvtColor(high2, cv2.COLOR_HSV2BGR))
+            cv2.imshow('nnmask', nnmask)
             cv2.imshow('img', img)
-            #cv2.imshow('mask', mask)
+            cv2.imshow('mask', mask)
             cv2.imshow('proc', res)
+            print(time.time() - t2, angle, distance, end = '\r')
+            t2 = time.time()
 
         angle = math.degrees(math.atan2(yball-ycen, xball-xcen))
         if angle < 0:
@@ -208,17 +227,16 @@ def detect_the_ball(anglenum, dist, sizenum, debug):
         else: 
             angle = 180 - angle
         angle = -angle
-        if debug:
-            print(t.time() - t2, angle, distance, end = '\r')
-            t2 = t.time()
+
         anglenum.value = angle
         dist.value = distance
         sizenum.value = ball_ret_size
         if cv2.waitKey(1) != -1:
             break
+        rawCapture.truncate(0)
         #t.sleep(1)
 
-    cap.release()
+    camera.close()
     cv2.destroyAllWindows()
     
 def ball_inc(xb, yb, rb):
